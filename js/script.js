c5a46510688ff9497fc971340a39976a9d592354
qsrdev <matteo.albertini747@gmail.com>
2025-04-28 16:39:53 +0200

    feat: minor fixes

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const lightBulb = document.getElementById("bulb");
 lightSwitch.addEventListener("click", function () {
   if (lightSwitch.classList.contains("on")) {
     lightSwitch.classList.remove("on");
-    lightSwitch.innerText = "Luce spenta";
+    lightSwitch.innerText = "Accendi la luce";
     lightBulb.src = "./img/white_lamp_1.png";
   } else {
     lightSwitch.classList.add("on");
-    lightSwitch.innerText = "Luce accesa";
+    lightSwitch.innerText = "Spegni la luce";
     lightBulb.src = "./img/yellow_lamp_1.png";
   }
 });
